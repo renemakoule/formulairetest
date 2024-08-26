2cfd5a6c22bc211d6ffcd39a6349776310d8585b
renemakoule <160953708+renemakoule@users.noreply.github.com>
2024-08-26 18:18:20 +0100

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Formulaire from "@/components/Formulaire";
+import Image from "next/image";
+import "./globals.css";
+
+export default function Home() {
+  return (
+    <main>
+      <Formulaire />
+    </main>
+  );
+}
