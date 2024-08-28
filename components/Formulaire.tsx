a4a6b7e66670f8c685fedfbf84b1c1db8ae0bbbb
renemakoule <160953708+renemakoule@users.noreply.github.com>
2024-08-28 15:56:34 +0100

    Update Formulaire.tsx

--- components/Formulaire.tsx
+++ components/Formulaire.tsx
@@ -8,6 +8,7 @@ import { FileUpload } from "./ui/file-upload";
 import { BackgroundGradient } from "./ui/background-gradient";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import Accordion from "./Accordion";
 
 // Schéma de validation Zod
 const formulaireSchema = z.object({
@@ -29,7 +30,38 @@ const formulaireSchema = z.object({
     )
     .nullable(),
 });
+//************************************************ */
+type AccordionItemProps = {
+  title: string;
+  children: React.ReactNode;
+};
 
+//*********************************************** */
+
+const AccordionItem: React.FC<AccordionItemProps> = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="border-b border-red-500">
+      <button
+        className="flex justify-between w-full p-4 text-left  text-teal-700 font-semibold focus:outline-none rounded-full"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {title}
+        <span className={`transform ${isOpen ? "rotate-45" : "rotate-0"} transition-transform text-xl font-bold`}>
+          +
+        </span>
+      </button>
+      {isOpen && (
+        <div className="p-4 text-gray-700 bg-gray-50">
+          {children}
+        </div>
+      )}
+    </div>
+  );
+};
+
+//*********************************************** */
 const Formulaire: React.FC = () => {
   const [file, setFile] = useState<File | null>(null);
   const [videoPreview, setVideoPreview] = useState<string | null>(null);
@@ -78,6 +110,7 @@ const Formulaire: React.FC = () => {
       targetRef.current?.pause();
     };
   };
+  //************************************************************** */
 
   useEffect(() => {
     if (videoPreview) {
@@ -93,12 +126,37 @@ const Formulaire: React.FC = () => {
     });
     console.log("Form data:", data);
   };
+  //********************************************************** */
 
+  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files ? Array.from(event.target.files) : [];
+
+    // Fusionner les fichiers existants avec les nouveaux fichiers
+    setSelectedFiles((prevFiles) => {
+      // Créer un nouvel ensemble pour éviter les doublons
+      const newFiles = [...prevFiles, ...files];
+
+      // Si vous souhaitez éviter les doublons basés sur le nom du fichier
+      const uniqueFiles = newFiles.reduce((acc, current) => {
+        if (!acc.find(file => file.name === current.name)) {
+          acc.push(current);
+        }
+        return acc;
+      }, [] as File[]);
+
+      return uniqueFiles;
+    });
+  };
+
+
+//*************************************************************** */
   return (
     <div className="flex flex-col lg:flex-row justify-center bg-gray-50 min-h-screen p-4">
   <div className="order-2 lg:order-1 bg-white shadow-md rounded-lg p-8 w-full h-full lg:w-3/4 lg:ml-8 mt-8 lg:mt-0 max-w-3xl">
     <h1 className="text-2xl font-semibold mb-6">
-      Formulaire d'enregistrement de nouveaux clients
+      Settings
     </h1>
     <form
       className="h-full lg:max-h-[80vh] lg:overflow-y-auto overflow-hidden"
@@ -118,20 +176,7 @@ const Formulaire: React.FC = () => {
         {errors.username && <p className="text-red-500 text-sm">{errors.username.message as React.ReactNode}</p>}
       </div>
 
-      <div className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="url">
-          URL <span className="text-red-500">*</span>
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="url"
-          type="url"
-          placeholder="Enter the URL of your social networks"
-          {...register("url")}
-        />
-        {errors.url && <p className="text-red-500 text-sm">{errors.url.message as React.ReactNode}</p>}
-      </div>
-
+      {/*****Input description */}
       <div className="mb-4">
         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
           Describe yourself in a few words <span className="text-red-500">*</span>
@@ -147,6 +192,7 @@ const Formulaire: React.FC = () => {
         )}
       </div>
 
+      {/****Input upload a welcome video */}
       <div className="mb-4">
         <label className="block mb-2 text-xl font-semibold">Upload a Welcome Video</label>
         <div
@@ -154,6 +200,7 @@ const Formulaire: React.FC = () => {
         >
           <input
             type="file"
+            name='video'
             accept=".mp4, .webm, .mov, .avi, .wmv, .flv, .mpeg, .avchd"
             className="absolute inset-0 w-full opacity-0 cursor-pointer"
             onChange={onFileChange}
@@ -179,7 +226,6 @@ const Formulaire: React.FC = () => {
           )}
           {file && <p className="text-gray-600">{file.name}</p>}
         </div>
-        {errors.file && <p className="text-red-500 text-sm">{errors.file.message as React.ReactNode}</p>}
         
         {videoPreview && (
           <div className="block lg:hidden mb-4 mt-4">
@@ -198,11 +244,84 @@ const Formulaire: React.FC = () => {
         )}
       </div>
 
+       
+       {/******Input URL */}
       <div className="mb-4">
-      <label className="block mb-2 text-xl font-semibold">Upload your file PDF</label>
-        <FileUpload />
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="url">
+          URL
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="url"
+          type="url"
+          placeholder="Enter the link of your website or social network"
+        />
       </div>
 
+      <div className="mb-4">
+      <AccordionItem title="click to add a link to your website or social network">
+         <input
+               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+               type="url"
+               name='url1'
+               placeholder="Enter the link of your website or social network"
+        />
+      </AccordionItem>
+      </div>
+      
+      {/****Input upload file text */}
+      <div className="mb-4">
+      <AccordionItem title="click to add your file">
+        <div className="relative w-full">
+          {/* Champ de fichier caché */}
+          <input
+            id="file-upload"
+            type="file"
+            name="file_text"
+            accept=".txt, .doc, .docx, .pdf"
+            multiple
+            className="hidden"
+            onChange={handleFileChange} // Ajouter le gestionnaire d'événement
+          />
+
+          {/* Bouton personnalisé */}
+          <label
+            htmlFor="file-upload"
+            className="flex items-center justify-center cursor-pointer shadow appearance-none border border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500 hover:bg-blue-500 hover:text-white transition duration-300"
+          >
+            {/* Icône de téléchargement */}
+            <svg
+              className="w-5 h-5 mr-2 text-gray-500"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 4a1 1 0 011-1h12a1 1 0 011 1v10a1 1 0 01-1 1h-3v2a1 1 0 01-1.707.707L10 14.414l-2.293 2.293A1 1 0 016 16v-2H3a1 1 0 01-1-1V4zm8 6h3L10 4.414 7 10h3v3a1 1 0 102 0v-3z"
+                clipRule="evenodd"
+              />
+            </svg>
+            Click Here / Drop File Here
+          </label>
+          
+          {/* Afficher les noms et tailles des fichiers */}
+          {selectedFiles.length > 0 && (
+            <div className="mt-2">
+              <ul>
+                {selectedFiles.map((file, index) => (
+                  <li key={index}>
+                    {file.name} - {(file.size / 1024).toFixed(2)} KB
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      </AccordionItem>
+    </div>
+       
+       {/****button submit from */}
       <Button type="submit">Submit</Button>
     </form>
   </div>
